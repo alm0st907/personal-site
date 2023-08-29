1ff214c4980acd23b601be9f2b0cdc7173471351
Garrett Rudisill <garrett.rudisill@gmail.com>
2023-08-29 14:03:13 -0700

    init commit to start page with react+tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className="text-3xl font-bold flex items-center justify-center">
+            This site is under construction :)
+        </h1>
+    </>
+  )
+}
+
+export default App
