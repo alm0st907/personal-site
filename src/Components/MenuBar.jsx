af7f0f79e473fdd68648f2e56619ba0eebe8ee38
Garrett Rudisill <garrett.rudisill@gmail.com>
2023-08-31 14:48:31 -0700

    fix links because react router lol

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 export default function MenuBar()
 {
     return(
@@ -6,15 +8,12 @@ export default function MenuBar()
             <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>Garrett Rudisill</span>
             <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg dark:text-white md:flex-row md:space-x-8 md:mt-0 md:border-0  ">
-                    <li>
-                        <a href="/construction"
-                           className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                           aria-current="page">Home</a>
-                    </li>
-                    <li>
-                        <a href="/about"
-                           className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-                    </li>
+                    <Link to={"/construction"} className='className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'>
+                        Home
+                    </Link>
+                    <Link to={"/about"} className='className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'>
+                        About
+                    </Link>
                     <li>
                         <a href="https://www.linkedin.com/in/garrett-rudisill/"
                            className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
