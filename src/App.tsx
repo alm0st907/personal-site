5ca42137d260b37d1d74c6612bdee45930c391a3
Garrett Rudisill <garrett.rudisill@gmail.com>
2025-02-14 15:47:05 -0800

    substack and remove a period

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ function App() {
     return (
         <>
             <div className="h-screen w-full content-center text-center justify-center bg-[#121212] text-[#e0e0e0]">
-                <h1 className="text-6xl pb-4">Hi, I'm Garrett Rudisill.</h1>
+                <h1 className="text-6xl pb-4">Hi, I'm Garrett Rudisill</h1>
                 <p>I am a full stack developer, busy with a day job.</p>
                 <p>At some point I'll turn this into something a bit more flashy, but this does the job.</p>
+                <a href={"https://anotherdeveloper.substack.com/"}>Substack</a>
                 <a href={"https://www.linkedin.com/in/garrett-rudisill/"} className={"underline text-blue-500"}>Connect
                     with me on LinkedIn</a>
             </div>
