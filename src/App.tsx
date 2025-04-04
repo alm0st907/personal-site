7ef11c68e1df4bee05b822d695f62da3aa2fa822
Garrett Rudisill <garrett.rudisill@gmail.com>
2025-04-04 09:07:16 -0700

    update with contribution chart because its neat

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-function App() {
+import GitHubCalendar from 'react-github-calendar'
 
+function App() {
     return (
         <>
-            <div className="h-screen w-full content-center text-center justify-center bg-[#121212] text-[#e0e0e0]">
+            <div className="h-screen w-full flex flex-col items-center justify-center bg-[#121212] text-[#e0e0e0]">
                 <h1 className="text-6xl pb-4">Hi, I'm Garrett Rudisill 👋</h1>
-                <p>I am a full stack developer, busy with a day job.</p>
-                <p>At some point I'll turn this into something a bit more flashy, but this does the job.</p>
-                <a href={"https://anotherdeveloper.substack.com/"} className={"underline text-blue-500"}>Substack</a>
+                <p>I am a senior full stack developer, busy with a day job.</p>
+                <p>I will make something flashier eventually, but this will do for now.</p>
                 <br/>
-                <a href={"https://www.linkedin.com/in/garrett-rudisill/"} className={"underline text-blue-500"}>Connect
-                    with me on LinkedIn</a>
+                <div className="max-w-max mx-auto flex flex-col items-center">
+                    <a href="https://github.com/alm0st907">
+                        <p>@alm0st907 on GitHub</p>
+                        <GitHubCalendar username="alm0st907"/>
+                    </a>
+                </div>
+                <br/>
+                <a href="https://www.linkedin.com/in/garrett-rudisill/" className="underline text-blue-500">Connect with
+                    me on LinkedIn</a>
+                <br/>
+                <a href="https://anotherdeveloper.substack.com/" className="underline text-blue-500">Substack</a>
             </div>
         </>
     )
