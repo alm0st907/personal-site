701180241097476d1debfda6823345ee30a1287d
Garrett Rudisill <garrett.rudisill@gmail.com>
2025-04-24 17:24:00 -0700

    add vercel analytics for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
 import GitHubCalendar from 'react-github-calendar'
+import {Analytics} from "@vercel/analytics/react";
 
 function App() {
     return (
-        <>
-            <div className="h-screen w-full flex flex-col items-center justify-center bg-[#121212] text-[#e0e0e0]">
-                <h1 className="text-6xl mx-2 pb-4">Hi, I'm Garrett Rudisill 👋</h1>
-                <div className="m-2">
-                    <p>I am a senior full stack developer, busy with a day job.</p>
-                    <p>I will make something flashier eventually, but this will do for now.</p>
-                </div>
-                <br/>
-                <div className="flex flex-col items-center m-2 p-2">
-                    <a href="https://github.com/alm0st907">
-                        <p>@alm0st907 on GitHub</p>
-                        <div className="w-[380px] sm:w-full overflow-auto">
-                            <GitHubCalendar username="alm0st907"/>
-                        </div>
-                    </a>
-                </div>
-                <br/>
-                <a href="https://www.linkedin.com/in/garrett-rudisill/" className="underline text-blue-500">Connect with me on LinkedIn</a>
-                <br/>
-                <a href="https://anotherdeveloper.substack.com/" className="underline text-blue-500">Substack</a>
+        <div className="h-screen w-full flex flex-col items-center justify-center bg-[#121212] text-[#e0e0e0]">
+            <h1 className="text-6xl mx-2 pb-4">Hi, I'm Garrett Rudisill 👋</h1>
+            <div className="m-2">
+                <p>I am a senior full stack developer, busy with a day job.</p>
+                <p>I will make something flashier eventually, but this will do for now.</p>
             </div>
-        </>
+            <br/>
+            <div className="flex flex-col items-center m-2 p-2">
+                <a href="https://github.com/alm0st907">
+                    <p>@alm0st907 on GitHub</p>
+                    <div className="w-[380px] sm:w-full overflow-auto">
+                        <GitHubCalendar username="alm0st907"/>
+                    </div>
+                </a>
+            </div>
+            <br/>
+            <a href="https://www.linkedin.com/in/garrett-rudisill/" className="underline text-blue-500">Connect with me
+                on LinkedIn</a>
+            <br/>
+            <a href="https://anotherdeveloper.substack.com/" className="underline text-blue-500">Substack</a>
+            <Analytics/>
+        </div>
     )
 }
 
